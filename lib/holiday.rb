b28b27d0c074a99c4578808a3697572071259b30
Sara Walker <sarawall1187@gmail.com>
2018-12-27 16:48:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
  holiday_hash.each do |season, holiday_name|
-   season[holiday_name].to_s.capitalize!.split.join
+   puts season[holiday_name].to_s.capitalize!.split.join
  end
 end
 
