461b63b1795a890a1789716bab8a9eda17e4befe
Sara Walker <sarawall1187@gmail.com>
2018-12-27 17:42:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holidays|  
   holidays.each do |holiday_name, supply_array|
     binding.pry
-   puts #"{holiday_name.to_s.capitalize!}"""#{supply_array}"
+   puts "#{holiday_name.to_s.capitalize!}"""#{supply_array}"
   end
  end
  holiday_hash
